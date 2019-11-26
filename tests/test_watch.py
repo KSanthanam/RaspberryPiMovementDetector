4d885bba4940f08a2cc83bcd1419876f8c92ec9d
KK Santhanam <kk.santhanam@finchain.ai>
2019-11-27 02:58:42 +1100

    test_echo

--- tests/test_watch.py
+++ tests/test_watch.py
@@ -4,3 +4,7 @@ class TestWatch():
     def test_trig(self):
         watch = Watch(23,24)
         assert watch.trigger_pin() == 23
+        
+    def test_echo(self):
+        watch = Watch(23,24)
+        assert watch.echo_pin() == 24
