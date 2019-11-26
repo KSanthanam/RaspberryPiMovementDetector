56cb393e9d1c7a8be7cc240bd713fd9306d7710f
KK Santhanam <kk.santhanam@finchain.ai>
2019-11-26 18:24:09 +1100

    Version 0.3

--- setup.py
+++ setup.py
@@ -1,16 +1,29 @@
 import setuptools
-# from distutils.core import setup
+
+with open("README.txt", "r") as fh:
+    long_description = fh.read()
+
+with open("LICENSE", "r") as fh:
+    license = fh.read()
 
 setuptools.setup(
   name='RaaspberryPiMovementDetector',
   packages=['RaaspberryPiMovementDetector'],
-  version='0.2',
+  version='0.3',
   license='MIT',
   description='Raspberry Movement Detector',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
   author='KK Santhanam',
   author_email='KK.Santhanam@gmail.com',
   url='https://github.com/KSanthanam/RaspberryPiMovementDetector',
   download_url='https://github.com/KSanthanam/RaspberryPiMovementDetector/archive/v_01.tar.gz',
+  classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+  ],
+  license=license,
   keywords=['RaspberryPi', 'RaspberryPi IoT']
 )
   # setup_requires=['wheel']
