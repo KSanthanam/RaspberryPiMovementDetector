0c4e21dee797bee6e19cac724f82a5b769dbcd6d
KK Santhanam <kk.santhanam@finchain.ai>
2019-11-26 16:56:28 +1100

    ci/cd

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ setup(
   url='https://github.com/KSanthanam/RaspberryPiMovementDetector',
   download_url='https://github.com/KSanthanam/RaspberryPiMovementDetector/archive/v_01.tar.gz',
   keywords=['RaspberryPi', 'RaspberryPi IoT'],
-  install_requires=[
-    'numpy',
-    'scrapeasy'
-    ],  
 )
+  # install_requires=[
+  #   'numpy',
+  #   'scrapeasy'
+  #   ],  
