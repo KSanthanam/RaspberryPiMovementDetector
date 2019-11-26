b25700cb16cd51c4343e31a34e0e86bd62a5abb7
KK Santhanam <kk.santhanam@finchain.ai>
2019-11-27 02:21:39 +1100

    Add pins

--- MovementDetector/Watch.py
+++ MovementDetector/Watch.py
@@ -1,3 +1,10 @@
 class Watch:
-  def __init__(self):
-    self.trig = 1
+  def __init__(self, trig, echo):
+    self._trig = trig
+    self._echo = echo
+  
+  def trigger_pin(self):
+    return self._trig
+
+  def echo_pin(self):
+    return self._echo
