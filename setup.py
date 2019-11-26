9f673111b0396e44ff78675fd2213f7ed69c3673
KK Santhanam <kk.santhanam@finchain.ai>
2019-11-26 18:04:41 +1100

    Setup version 0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 setuptools.setup(
   name='RaaspberryPiMovementDetector',
   packages=['RaaspberryPiMovementDetector'],
-  version='0.1',
+  version='0.2',
   license='MIT',
   description='Raspberry Movement Detector',
   author='KK Santhanam',
