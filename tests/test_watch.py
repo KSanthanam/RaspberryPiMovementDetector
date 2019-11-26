4c92dfb2e803da657b3d551191fe03cf5a0143fa
KK Santhanam <kk.santhanam@finchain.ai>
2019-11-27 02:50:58 +1100

    trigger_pin()

--- tests/test_watch.py
+++ tests/test_watch.py
@@ -3,4 +3,4 @@ from MovementDetector.Watch import Watch
 class TestWatch():
     def test_trig(self):
         watch = Watch(23,24)
-        assert watch.trigger_pin == 23
+        assert watch.trigger_pin() == 23
