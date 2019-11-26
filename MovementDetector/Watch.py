73c2b295ef7e4cbb5fc9cb66e6f6304b39c23d5d
KK Santhanam <kk.santhanam@finchain.ai>
2019-11-27 03:55:20 +1100

    get_distance

--- MovementDetector/Watch.py
+++ MovementDetector/Watch.py
@@ -1,3 +1,5 @@
+import RPi.GPIO as GPIO
+import time
 class Watch:
   def __init__(self, trig, echo):
     self._trig = trig
@@ -8,3 +10,31 @@ class Watch:
 
   def echo_pin(self):
     return self._echo
+
+  def get_distance(self):
+    print "Distance Measurement In Progress"
+    GPIO.setup(self._trig, GPIO.OUT)
+    GPIO.setup(self._echo, GPIO.IN)
+    GPIO.output(self._trig, False)
+    print "Waiting For Sensor To Settle"
+    time.sleep(2)
+
+    GPIO.output(self._trig, True)
+    time.sleep(0.00001)
+    GPIO.output(self._trig, False)
+
+    while GPIO.input(self._echo)==0:
+      pulse_start = time.time()
+
+    while GPIO.input(self._echo)==1:
+      pulse_end = time.time()
+
+    pulse_duration = pulse_end - pulse_start
+
+    distance = pulse_duration * 17150
+    distance = round(distance, 2)
+
+    print "Distance:",distance,"cm"
+
+    GPIO.cleanup()
+    
