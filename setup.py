b209f0c7e3a990c050f37defda5af7311612ee01
KK Santhanam <kk.santhanam@finchain.ai>
2019-11-28 03:35:12 +1100

    v0.11

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("LICENSE", "r") as fh:
 setup(
   name='RaspberryPiMovementDetector',
   packages=find_packages(),
-  version='0.10',
+  version='0.11',
   description='Raspberry Movement Detector',
   long_description=long_description,
   requires=['wheel'],
